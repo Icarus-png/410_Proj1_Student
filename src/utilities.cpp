e96e5ca7c68747b0241449a64a3ca83f9fdfee54
Josh <joshua.beck.17@cnu.edu>
2020-09-17 17:08:43 -0400

    Finished loadData; Needs comments

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -16,6 +16,8 @@ using namespace std;
 
 //********************** private to this compilation unit **********************
 
+vector<process_stats> stats;
+
 //if myString does not contain a string rep of number returns o
 //if int not large enough has undefined behaviour, very fragile
 int stringToInt(const char *myString) {
@@ -23,6 +25,41 @@ int stringToInt(const char *myString) {
 }
 
 int loadData(const char* filename, bool ignoreFirstRow) {
+
+	stats.clear();
+	string str;
+
+	ifstream inFile;
+	inFile.open(filename);
+
+	if (!inFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	if (ignoreFirstRow) {
+		getline(inFile, str);
+	}
+
+	while (getline(inFile, str)) {
+		stringstream stream(str);
+		vector<string>  vals;
+		string temp;
+
+		while (getline(stream, temp, CHAR_TO_SEARCH_FOR)) {
+			vals.push_back(temp);
+		}
+
+		if (vals.size() == 4 && none_of(vals.begin(), vals.end(), " ") && none_of(vals.begin(), vals.end(), "")) {
+			process_stats ps;
+
+			ps.cpu_time       = stringToInt(vals[0].c_str());
+			ps.process_number = stringToInt(vals[1].c_str());
+			ps.start_time     = stringToInt(vals[2].c_str());
+			ps.io_time        = stringToInt(vals[3].c_str());
+
+			stats.push_back(ps);
+		}
+	}
 
 	return SUCCESS;
 }
@@ -41,7 +78,7 @@ process_stats getNext() {
 
 //returns number of process_stats structs in the vector holding them
 int getNumbRows(){
-	return 0;
+	return stats.size();
 }
 
 
